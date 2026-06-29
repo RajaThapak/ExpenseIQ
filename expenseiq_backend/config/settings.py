d785910d514fe4937e67cf20e049b44312b2c15e
RajaThapak <msthapakraja10@gmail.com>
2026-06-29 06:16:37 +0000

    add filtering and searching api

--- expenseiq_backend/config/settings.py
+++ expenseiq_backend/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'users',
     "categories",
     "expenses",
@@ -128,4 +129,13 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
 }
--- expenseiq_backend/expenses/views.py
+++ expenseiq_backend/expenses/views.py
@@ -3,6 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Expense
 from .serializers import ExpenseSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
+from .filters import ExpenseFilter
 
 
 class ExpenseCreateView(generics.CreateAPIView):
@@ -19,7 +23,30 @@ class ExpenseListView(generics.ListAPIView):
     serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
 
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+
+    filterset_class = ExpenseFilter
+
+    search_fields = [
+        "title",
+        "notes",
+    ]
+
+    ordering_fields = [
+        "amount",
+        "date",
+        "created_at",
+    ]
+
+    ordering = [
+        "-date",
+    ]
+
     def get_queryset(self):
         return Expense.objects.filter(
             user=self.request.user
-        ).order_by("-date")
+        )
